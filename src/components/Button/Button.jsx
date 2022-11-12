86e9cfe31757ed7158234d3f9e087a034a244682
Oliver Janhunen <oliver.janhunen@gmail.com>
2022-11-12 11:42:08 +0200

    added utilities folder with css reset and a variables file in it. imported reset.scss into index.scss. changed app.css and index.css into scss files

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card__h1">test</div>
+      <div className="card__wrapper">
+        <h1 className="card__h1--error">test</h1>
+      </div>
+    </div>
+  );
 };
 
-export default Button;
+export default Card;
