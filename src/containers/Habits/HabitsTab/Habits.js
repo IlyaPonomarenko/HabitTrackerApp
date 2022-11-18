5206362195f8eb254f366366fe97bcdd3072a4b2
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-11-18 14:33:06 +0200

    added weekdays to the calendar frames

--- src/containers/Habits/HabitsTab/Habits.js
+++ src/containers/Habits/HabitsTab/Habits.js
@@ -11,7 +11,17 @@ return (
             <HabitGroup></HabitGroup>
             <HabitGroup></HabitGroup>
         </div>
-        <div className="habits-calendar"></div>
+        <div className="habits-calendar">
+            <div className="habits-calendar__weekdays">
+                <div className="day">Mon</div>
+                <div className="day">Thu</div>
+                <div className="day">Wen</div>
+                <div className="day">Tue</div>
+                <div className="day">Fri</div>
+                <div className="day">Sat</div>
+                <div className="day">Sun</div>
+            </div>
+        </div>
     </div>
 )
 };
