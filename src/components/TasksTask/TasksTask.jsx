8c4896391d9e87f462ec6dfb74a19385fa98a97d
Oliver Janhunen <oliver.janhunen@gmail.com>
2022-12-02 10:18:44 +0200

    added functioning priority picker

--- src/components/TasksTask/TasksTask.jsx
+++ src/components/TasksTask/TasksTask.jsx
@@ -1,41 +1,49 @@
 import "./TasksTask.scss";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import Checkbox from "../Checkbox/Checkbox";
+import PriorityPicker from "../PriorityPicker/PriorityPicker";
 
 const TasksTask = ({
   taskDate = "Enter Date Here",
   taskTitle = "Task Title",
   taskIsActive = true,
-  onTaskClick,
   taskContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quasi blanditiis id quisquam repellendus ipsa iusto fugit laboriosam, ad nostrum.",
   taskDaysMissed = 3,
 }) => {
+  const [showTask, setShowTask] = useState(true);
+  const showTaskHandler = () => {
+    setShowTask(!showTask);
+  };
   return (
-    <div className="task" onClick={onTaskClick}>
+    <div className={showTask ? "task--active" : "task"}>
       <div className="task__header">
         <Checkbox />
-        <div className="task__title">
+        <div className="task__title" onClick={showTaskHandler}>
           <span className="task__label">{taskDate}</span>
           <h3 className="task__h3">{taskTitle}</h3>
         </div>
         <div className="task__trailing">
           <div className="task__dot"></div>
 
-          {taskIsActive ? (
-            <span className="material-icons">arrow_drop_down</span>
+          {showTask ? (
+            <span className="material-icons" onClick={showTaskHandler}>
+              arrow_drop_down
+            </span>
           ) : (
-            <span className="material-icons">arrow_right</span>
+            <span className="material-icons" onClick={showTaskHandler}>
+              arrow_right
+            </span>
           )}
         </div>
         {/* Change arrow icon if task is active */}
       </div>
       {/* Display content if task is active */}
-      {taskIsActive && (
+      {showTask && (
         <>
           <div className="task__content">{taskContent}</div>
           <div className="task__buttons-wrapper">
-            <Button icon={"import_export"}>Priority</Button>
+            <PriorityPicker />
             <Button icon={"calendar_month"}>Add Date</Button>
             <Button icon={"bookmark"}>Remind Me</Button>
             <Button icon={"timer"}>Length of task</Button>
