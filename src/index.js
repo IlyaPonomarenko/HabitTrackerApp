b0979a5d8fdc933bdae0ef8951427327529e1d9f
Oliver Janhunen <oliver.janhunen@gmail.com>
2022-12-14 17:34:52 +0200

    clean commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 /* Import Pages */
 import App from "./App";
-/* Will be changed to ./containers/Habits/Habits */
-import Habits from "./containers/HabitsPlaceholder/Habits";
-/* Will be changed to ./containers/Journal/Journal */
-import Journal from "./JournalMain";
 import Tasks from "./containers/Tasks/Tasks";
 import Error404 from "./components/Error404/Error404";
 
@@ -22,8 +18,6 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/" element={<Habits />} />
-        <Route path="journal" element={<Journal />} />
         <Route path="tasks" element={<Tasks />} />
       </Route>
       {/* TODO: Replace with actual 404 page */}
